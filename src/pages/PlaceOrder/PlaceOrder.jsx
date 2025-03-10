610cdba7366334c5b530fad1215ce2305473eb18
nhi_2151220246@dau.edu.vn <nguyenthilinhnhi0000@gmail.com>
2025-03-10 21:59:53 +0700

    create place order page

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext';
 const PlaceOrder = () => {
-  return (
-    <div>
 
-    </div>
+  const { getTotalCartAmount } = useContext(StoreContext);
+  return (
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delevery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Street Number" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="Phone" />
+        </div>
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
